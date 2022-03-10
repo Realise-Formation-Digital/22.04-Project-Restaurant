0488d9be609206dceb85a5a4eee147b50355cb47
Xavier <dejuan.xavier@gmail.com>
2022-03-10 10:37:54 +0100

    Projet_Restaurant_Javascript

---
+++ main.js
@@ -0,0 +1,55 @@
+// Tentative d'optimisation du code en utilisant des listes
+
+//------------création de tableau et récupération du clique
+
+const tableaudeLiens = document.getElementsByClassName("nav-link");
+
+for (element of tableaudeLiens) {
+
+console.log(element)
+
+    element.addEventListener("click", function (e) {
+        console.log('click', e.target.id)
+
+//------------Tentative de faire le lien de destination (pas trouvé du tout)        
+
+        let destination = element.id[i]
+        console.log(destination)
+        
+//------------Scroll
+
+        destination.scrollIntoView();
+        destination.scrollIntoView(false);
+        destination.scrollIntoView({block: "end"});
+        destination.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+})
+}
+
+  
+
+
+
+
+// Ancien code fuctionnelle
+
+// ----------------Création de la variable et écoute du clique
+
+//      let stock = document.getElementById("home");
+
+//      stock.addEventListener("click", function(e) {
+//  
+//-----------------Création de la variable contenant la destination.
+
+//         let destination = document.getElementById("about-us");
+
+//-----------------Scroll de destination
+
+//         destination.scrollIntoView();
+//         destination.scrollIntoView(false);
+//         destination.scrollIntoView({block: "end"});
+//         destination.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+        
+//         console.log('click', e.target.id);
+//     })
+
+
