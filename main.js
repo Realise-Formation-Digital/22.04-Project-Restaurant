1960e04010176d5433472058a11c840970372b32
Xavier <dejuan.xavier@gmail.com>
2022-03-10 12:03:06 +0100

    UpdateJS_Projet_Restaurant_Javascript
    Correctifs des erreurs de scroll et derniers commentaires

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
+// Les erreurs après présentation sont passées en commentaires
+
 // Tentative d'optimisation du code en utilisant des listes
+
+
 
 //------------création de tableau et récupération du clique
 
-const tableaudeLiens = document.getElementsByClassName("nav-link");
+// const tableaudeLiens = document.getElementsByClassName("nav-link");
 
-for (element of tableaudeLiens) {
+// for (element of tableaudeLiens) {
 
-console.log(element)
+// console.log(element)
 
-    element.addEventListener("click", function (e) {
-        console.log('click', e.target.id)
+//     element.addEventListener("click", function (e) {
+//         console.log('click', e.target.id)
 
 //------------Tentative de faire le lien de destination (pas trouvé du tout)        
 
-        let destination = element.id[i]
-        console.log(destination)
+        // let destination = element.id[i]
+        // console.log(destination)
         
 //------------Scroll
 
-        destination.scrollIntoView();
-        destination.scrollIntoView(false);
-        destination.scrollIntoView({block: "end"});
-        destination.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-})
-}
+//         destination.scrollIntoView();
+//         destination.scrollIntoView(false);
+//         destination.scrollIntoView({block: "end"});
+//         destination.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+// })
+// }
 
   
 
@@ -34,22 +38,22 @@ console.log(element)
 
 // ----------------Création de la variable et écoute du clique
 
-//      let stock = document.getElementById("home");
+     let stock = document.getElementById("home");
 
-//      stock.addEventListener("click", function(e) {
+     stock.addEventListener("click", function(e) {
 //  
 //-----------------Création de la variable contenant la destination.
 
-//         let destination = document.getElementById("about-us");
+        let destination = document.getElementById("about-us");
 
 //-----------------Scroll de destination
 
-//         destination.scrollIntoView();
-//         destination.scrollIntoView(false);
-//         destination.scrollIntoView({block: "end"});
-//         destination.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+        // destination.scrollIntoView(); inutile
+        // destination.scrollIntoView(false); inutile
+        // destination.scrollIntoView({block: "end"}); inutile 
+        destination.scrollIntoView({behavior: "smooth" });
         
-//         console.log('click', e.target.id);
-//     })
+        // console.log('click', e.target.id);
+    })
 
 
